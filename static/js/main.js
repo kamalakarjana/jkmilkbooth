ccf6a79d3c68a3a94e9946e1e8882197531fd6c0
kamalakarjana <your-email@example.com>
2026-07-09 22:20:01 +0530

    commit

--- static/js/main.js
+++ static/js/main.js
@@ -45,7 +45,7 @@ function exportTableToPDF(elementId, filename = 'export.pdf') {
         filename: filename,
         image: { type: 'jpeg', quality: 0.98 },
         html2canvas: { scale: 2 },
-        jsPDF: { orientation: 'portrait', unit: 'mm', format: 'a4' }
+        jsPDF: { orientation: 'landscape', unit: 'mm', format: 'a4' }
     };
     html2pdf().set(opt).from(element).save();
 }
